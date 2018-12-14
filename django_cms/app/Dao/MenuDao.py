b45d3cc2b03143ee322de5b8dc932bab17730f89
dasgk <674026565@qq.com>
2018-12-14 22:27:40 +0800

    提交代码

---
+++ django_cms/app/Dao/MenuDao.py
@@ -0,0 +1,9 @@
+class MenuDao:
+    def getMenuList(self):
+        return [
+            {
+                'text': '测试菜单',
+                'icon_class': '',
+                'url': 'http://www.baidu.com'
+            },
+        ];
