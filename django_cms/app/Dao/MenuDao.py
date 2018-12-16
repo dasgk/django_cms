05174cb07d9820ee53304181967811f3c90d98a8
dasgk <674026565@qq.com>
2018-12-16 19:58:48 +0800

    初始化菜单选项

--- django_cms/app/Dao/MenuDao.py
+++ django_cms/app/Dao/MenuDao.py
@@ -1,9 +1,22 @@
 class MenuDao:
-    def getMenuList(self):
+    @staticmethod
+    def getMenuListByUser():
+        menu_list = MenuDao.getMenuList()
+        return menu_list
+
+    @staticmethod
+    def getMenuList():
         return [
             {
+                'id': 'menu-article',
                 'text': '测试菜单',
                 'icon_class': '',
-                'url': 'http://www.baidu.com'
+                'url': 'http://www.baidu.com',
+                'nodes':[
+                    {
+                        'url':'http://www.baidu.com',
+                        'title':'测试菜单'
+                    },
+                ]
             },
         ];
--- django_cms/app/Http/Controller/HomeController.py
+++ django_cms/app/Http/Controller/HomeController.py
@@ -1,7 +1,11 @@
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django_cms.app.Dao.MenuDao import MenuDao
+from django.template import Context
 
 def form(request):
+    menu_list = MenuDao.getMenuListByUser()
+    context = {'menu_list': menu_list}
     t = get_template('admin/index.html')
-    html = t.render()
+    html = t.render(context)
     return HttpResponse(html)
