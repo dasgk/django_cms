cc6252251a121b4becd8b47a1dc226e1ad2cbb89
建业周 <jianyezhou@jianyezhoudeMacBook-Pro.local>
2019-01-29 21:03:23 +0800

    修改

---
+++ django_cms/app/Http/Controller/Admin/HomeController.py
@@ -0,0 +1,31 @@
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django_cms.app.Dao.MenuDao import MenuDao
+from django.views import View
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
+
+class HomeController(View):
+    # 网站首页
+    def index(request):
+
+        menu_list = MenuDao.getMenuListByUser()
+        context = {'menu_list': menu_list,'user_name':request.user.username}
+        t = get_template('admin/admin_index.html')
+        html = t.render(context)
+        return HttpResponse(html)
+
+    def jump2index(request):
+        return HttpResponseRedirect('/admin/index')
+
+    # 欢迎致辞
+    def welcome(request):
+        t = get_template('admin/welcome.html')
+        html = t.render()
+        return HttpResponse(html)
+
+    def logout(request):
+        auth.logout(request)
+        return HttpResponseRedirect("/admin/login/")
