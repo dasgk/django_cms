7e9624203e4d0a573262cf9106df0de3935437ce
zhoujianye <111@123.com>
2019-02-22 15:43:20 +0800

    添加django的csrf功能

--- django_cms/app/Http/Controller/Admin/UserController.py
+++ django_cms/app/Http/Controller/Admin/UserController.py
@@ -2,11 +2,22 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 from django.views import View
 
+from django.shortcuts import render_to_response,render
+from django.template import RequestContext
 
-class UserController(View):
+from django.views.generic import TemplateView
+class UserController(TemplateView):
     # 网站首页
+
     def login(request):
-        context = {}
-        t = get_template('admin/login.html')
-        html = t.render(context)
-        return HttpResponse(html)
+        if request.method == 'GET':
+            return render(request, 'admin/login.html', {'article_list': []})
+        else:
+            return render(request, 'admin/index.html', {'article_list': []})
+            parm = request.POST
+            new_user = UserInfo()
+            new_user.user_name = parm.get('user_name', 'not found')
+            new_user.password = parm.get('pwd', 'not found')
+            new_user.email = parm.get('email', 'not found')
+
+
--- django_cms/settings.py
+++ django_cms/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -37,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cms',  #这里是自己定义的模型类的路径
+    'django_cms',  # 这里是自己定义的模型类的路径
 ]
 
 MIDDLEWARE = [
@@ -45,35 +44,34 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_cms.app.Http.Middleware.AuthMiddleware.AuthMiddleWare'
 ]
 ROOT_URLCONF = 'django_cms.urls'
-MIGRATION_MODULES =[
+MIGRATION_MODULES = [
     'django_cms.migrations'
 ]
 
-
 TEMPLATES = [
-		{
-			'BACKEND': 'django.template.backends.django.DjangoTemplates',
-			'DIRS': [BASE_DIR+"/django_cms/resource/views"],
-			'APP_DIRS': True,
-			'OPTIONS': {
-				'context_processors': [
-					'django.template.context_processors.debug',
-					'django.template.context_processors.request',
-					'django.contrib.auth.context_processors.auth',
-					'django.contrib.messages.context_processors.messages',
-				],
-			},
-		},
-	]
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR + "/django_cms/resource/views"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'django_cms.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -88,7 +86,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -108,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -121,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
@@ -136,16 +131,16 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
         },
     },
     'loggers': {
         'django.db.backends': {
             'handlers': ['console'],
             'propagate': True,
-            'level':'DEBUG',
+            'level': 'DEBUG',
         },
     }
 }
