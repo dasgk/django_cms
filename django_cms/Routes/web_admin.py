140af3f94b063b5fa332565615a187219e3fa825
建业周 <jianyezhou@jianyezhoudeMacBook-Pro.local>
2019-02-24 10:41:31 +0800

    修改文件夹名称

--- django_cms/Routes/web_admin.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django_cms.app.Http.Controller.Admin.HomeController import HomeController
-from django_cms.app.Http.Controller.Admin.UserController import UserController
-
-# 如果
-urlpatterns = [
-    url(r'user/login', UserController.login, name='admin.user.login'),
-    url(r'index', HomeController.index, name='admin.index')
-]
