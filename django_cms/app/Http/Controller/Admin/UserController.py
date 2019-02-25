8fac645836551aa31fd75378b33cd27857cb464f
zhoujianye <111@123.com>
2019-02-25 13:47:49 +0800

    修改缩进

--- django_cms/app/Http/Controller/Admin/UserController.py
+++ django_cms/app/Http/Controller/Admin/UserController.py
@@ -1,17 +1,16 @@
-from django.template.loader import get_template
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views import View
-
-from django.shortcuts import render_to_response, render, redirect
-
+from django.shortcuts import  render
 from django_cms.models import AdminUser
 from django.views.generic import TemplateView
 from django_cms.app.utility.helps import set_login, is_login, do_logout, response_json
 
 
 class UserController(TemplateView):
-    # 网站首页
 
+    '''
+        用户登录操作，GET请求显示登录页面，判断已登录，跳转到首页
+                     POST请求，判断输入用户名密码是否正确
+    '''
     def login(request):
         if request.method == 'GET':
             admin_user = is_login(request)
@@ -32,9 +31,9 @@ class UserController(TemplateView):
                 return HttpResponseRedirect('/index')
             else:
                 return HttpResponse('error')
-
+    '''
+        用户登出操作，删除session的值
+    '''
     def logout(request):
         do_logout(request)
-        result = {"status": "错误", "data": "", "city": "北京"}
-        # json返回为中文
         return response_json(1, "")
