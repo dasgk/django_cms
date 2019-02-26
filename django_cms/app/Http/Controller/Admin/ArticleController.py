afa1c3408666b45216b04f63448c75f5c5c956bc
dasgk <674026565@qq.com>
2019-02-26 22:01:08 +0800

    添加文章管理

--- django_cms/app/Http/Controller/Admin/ArticleController.py
+++ django_cms/app/Http/Controller/Admin/ArticleController.py
@@ -8,7 +8,18 @@ class ArticleController(View):
     # 网站首页
     def index(request):
         # 获得所有数据
-        article_list = Article.objects.all()
+        param = request.GET
+        type_id = param.get('type_id',0)
+        title = param.get('title','')
+        filter_dict = dict()
+
+        if len(title)>0:
+            title = "%"+title+"%"
+            filter_dict['title_contains'] = title
+        if type_id:
+            filter_dict['type_id'] = type_id
+
+        article_list = Article.objects.filter(**filter_dict).all()
         # 进行分页操作
         p = Paginator(article_list, 3)
         page = request.GET.get('page')
@@ -22,7 +33,7 @@ class ArticleController(View):
         except EmptyPage:
             # If page is out of range (e.g. 9999), deliver last page of results.
             article_list = p.page(1)
-        context = {'article_list': article_list,'user_name':request.user.username}
+        context = {'article_list': article_list}
         t = get_template('admin/article.html')
         html = t.render(context)
         return HttpResponse(html)
