762dc33d4c9751cefc7554264db5b4039e1272e1
zhoujianye <111@123.com>
2019-03-02 09:39:21 +0800

    添加分类管理

--- django_cms/models.py
+++ django_cms/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class AdminUser(models.Model):
     admin_user_id = models.IntegerField(primary_key=True, help_text="主键")
     username = models.CharField(max_length=200, help_text="管理员的用户名")
@@ -18,11 +17,21 @@ class Article(models.Model):
     article_id = models.IntegerField(primary_key=True, help_text="主键")
     title = models.CharField(max_length=200, help_text="文章标题", null=True, default="")
     content = models.TextField(help_text="文章内容", null=True)
-    look_num = models.IntegerField(help_text="浏览数量",default=0, null=True)
-    type = models.IntegerField(help_text="文章类型", default=0, null=True)
+    look_num = models.IntegerField(help_text="浏览数量", default=0, null=True)
+    cate_id = models.IntegerField(help_text="文章类别", default=0, null=True)
     status = models.IntegerField(help_text="1 表示草稿 2表示已经发表", default=1, null=True)
     created_at = models.DateTimeField(help_text="创建时间", null=True, default="2019-01-29 12:13:00", blank=True)
-    updated_at = models.DateTimeField(help_text="更新时间",null=True,default="2019-01-29 12:13:00")
+    updated_at = models.DateTimeField(help_text="更新时间", null=True, default="2019-01-29 12:13:00")
 
     class Meta:
         db_table = "article"
+
+
+class Cate(models.Model):
+    cate_id = models.IntegerField(primary_key=True, help_text="主键")
+    title = models.CharField(max_length=200, help_text="类别描述", null=True, default="")
+    created_at = models.DateTimeField(help_text="创建时间", null=True, default="2019-01-29 12:13:00", blank=True)
+    updated_at = models.DateTimeField(help_text="更新时间", null=True, default="2019-01-29 12:13:00")
+
+    class Meta:
+        db_table = "cate"
