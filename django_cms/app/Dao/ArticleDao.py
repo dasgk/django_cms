5bf9977539491210f15d19f19446418495a74d54
zhoujianye <111@123.com>
2019-03-08 17:16:19 +0800

    添加标签处理

--- django_cms/app/Dao/ArticleDao.py
+++ django_cms/app/Dao/ArticleDao.py
@@ -1,26 +1,32 @@
 from django_cms.app.Dao.ConstDao import ConstDao
-from django_cms.models import Article,LabelArticle
+from django_cms.models import Article, LabelArticle
+
+
 class ArticleDao(object):
     @staticmethod
     def getArticleList(options, page_num, label_id):
         if label_id:
             label_filter = dict()
             label_filter['label_id'] = label_id
-            article_ids = LabelArticle.objects.filter(**label_filter).values_list('article_id',flat=True)
+            article_ids = LabelArticle.objects.filter(**label_filter).values_list('article_id', flat=True)
             if article_ids:
                 options['article_id__in'] = article_ids
-            else :
-                return [[],0];
+            else:
+                return [[], 0];
         skip = (int(page_num) - 1) * ConstDao.getPageNum()
         article_list = Article.objects.filter(**options).order_by("article_id").all()[skip:skip + ConstDao.getPageNum()]
         article_count = Article.objects.filter(**options).count()
         return [article_list, article_count]
 
     @staticmethod
-    def createOrUpdate(article_id, title, content,cate_id):
-        article_filter = dict()
-        article_filter['article_id'] = article_id
-        article = Article.objects.filter(**article_filter).first()
+    def createOrUpdate(article_id, title, content, cate_id):
+        if type(article_id) != int and len(article_id) == 0:
+            article_id = 0
+        article = None
+        if article_id > 0:
+            article_filter = dict()
+            article_filter['article_id'] = article_id
+            article = Article.objects.filter(**article_filter).first()
 
         if article == None:
             article = Article()
@@ -34,5 +40,3 @@ class ArticleDao(object):
             article.cate_id = cate_id
             article.save()
         return article
-
-
--- django_cms/app/Http/Controller/Admin/HomeController.py
+++ django_cms/app/Http/Controller/Admin/HomeController.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.views import View
 from django_cms.app.utility.helps import is_login
+from django_cms.models import  Article,Label,Cate
 
 class HomeController(View):
     # 网站首页
@@ -10,7 +11,9 @@ class HomeController(View):
         admin_user = is_login(request)
         if admin_user != False:
             t = get_template('admin/index.html')
-            html = t.render({'user':admin_user})
+            cate_count = Cate.objects.count()
+            label_count = Label.objects.count()
+            html = t.render({'user':admin_user,'cate_count':cate_count,'label_count':label_count})
             return HttpResponse(html)
         else:
             return HttpResponseRedirect('/user/login')
