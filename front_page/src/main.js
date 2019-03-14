68d25d5eaff31626eee9426cd1ba674096ef1a86
dasgk <674026565@qq.com>
2019-03-14 22:30:18 +0800

    添加vue支持

--- front_page/src/main.js
+++ front_page/src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+
+Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
