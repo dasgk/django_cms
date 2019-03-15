700aa27b2ea31ee427e1460fed51d474ee5c841d
zhoujianye <111@123.com>
2019-03-15 14:56:16 +0800

    修改vue

--- front_page/src/main.js
+++ front_page/src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
+import App from './components/Index.vue';
 
 Vue.use(ElementUI);
 
