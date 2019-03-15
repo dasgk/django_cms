5f115cf52a2eb7d3039c67cb86b48652fac0e147
zhoujianye <111@123.com>
2019-03-15 17:28:28 +0800

    修改布局

--- front_page/src/components/Index.vue
+++ front_page/src/components/Index.vue
@@ -7,19 +7,34 @@
 			</el-aside>
 			<!--占位使用，不做任何处理-->
 			<el-main></el-main>
-			<el-aside >
+			<el-aside>
 				<!--搜索内容开始-->
 				<el-row style="height: 17%;">
-					<el-col :span="12" style="    margin-top: 12%; margin-left: 5%;">						
-							<input type="text" autocomplete="off" placeholder="请输入内容" class="el-input__inner">						
+					<el-col :span="12" style="    margin-top: 12%; margin-left: 5%;">
+						<input type="text" autocomplete="off" placeholder="请输入内容" class="el-input__inner">
 					</el-col>
-					<el-col :span="12"     style="margin-top: -12.8%;   margin-left: 59%;">
-						
-							<el-button type="primary" icon="el-icon-search">搜索</el-button>
-						
+					<el-col :span="12" style="margin-top: -12.8%;   margin-left: 59%;">
+						<el-button type="primary" icon="el-icon-search">搜索</el-button>
 					</el-col>
-					
 				</el-row>
+				<!--搜索内容结束-->
+
+				<!--我的分类开始-->
+				<el-row style="    margin-top: 25%;">
+					<el-col :span="24" style=" text-align:center;height: 30%;    line-height: 294%;">
+						<div class="grid-content bg-purple-dark"><span style='    color: black;font-size: 22px;'>我的分类</span></div>
+					</el-col>
+					<el-col :span="24" style=" text-align:center; line-height: 294%;">
+						<div class="grid-content bg-purple-dark"><span style='    color: grey;font-size: 22px;'>我的分类</span></div>
+					</el-col>
+					<el-col :span="24" style=" text-align:center; line-height: 294%;">
+						<div class="grid-content bg-purple-dark"><span style='    color:grey;font-size: 22px;'>我的分类</span></div>
+					</el-col>
+					<el-col :span="24" style=" text-align:center; line-height: 294%;">
+						<div class="grid-content bg-purple-dark"><span style='    color: grey;font-size: 22px;'>我的分类</span></div>
+					</el-col>
+				</el-row>
+				<!--我的分类结束-->
 			</el-aside>
 			<!--占位使用，不做任何处理-->
 			<el-aside>
@@ -41,7 +56,7 @@
 			margin-bottom: 0;
 		}
 	}
-
+	
 	.el-input {
 		height: 10px;
 	}
@@ -49,10 +64,12 @@
 	.el-col {
 		border-radius: 4px;
 		margin: auto;
-	}	
-	.el-aside{
-		    overflow: hidden;
 	}
+	
+	.el-aside {
+		overflow: hidden;
+	}
+	
 	body {
 		background: #f6f6f6;
 	}
