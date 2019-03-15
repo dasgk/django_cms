e9de50e22f5cab9be562dfb3ba740a22c91dc7b6
zhoujianye <111@123.com>
2019-03-15 15:29:22 +0800

    首页布局文件

--- front_page/src/components/Index.vue
+++ front_page/src/components/Index.vue
@@ -1,44 +1,30 @@
 <template>
-
-<el-container style="height:100%" direction="vertical">
+<el-container style=" position: absolute;top: 0;left: 0;width: 100%; height: 100%;">
   <el-header>Header</el-header>
+  <el-container>
   <el-main>Main</el-main>
-  <el-footer>Footer</el-footer>
-</el-container>
+    <el-aside width="200px">Aside</el-aside>
 
+  </el-container>
+   <el-footer>Footer</el-footer>
+</el-container>
 </template>
 <style>
   .el-header, .el-footer {
     background-color: #B3C0D1;
     color: #333;
     text-align: center;
-    line-height: 60px;
+    height:10%;
   }
-
   .el-aside {
     background-color: #D3DCE6;
     color: #333;
     text-align: center;
-    line-height: 200px;
   }
-
   .el-main {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
-    line-height: 160px;
-  }
 
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
   }
 </style>
