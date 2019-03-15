caa9446ec1da2306d12277b9e7f403ea70928001
zhoujianye <111@123.com>
2019-03-15 15:31:36 +0800

    首页布局文件

--- front_page/src/components/Index.vue
+++ front_page/src/components/Index.vue
@@ -1,12 +1,11 @@
 <template>
 <el-container style=" position: absolute;top: 0;left: 0;width: 100%; height: 100%;">
-  <el-header>Header</el-header>
+  <el-header style="height:13%;">Header</el-header>
   <el-container>
   <el-main>Main</el-main>
     <el-aside width="200px">Aside</el-aside>
-
   </el-container>
-   <el-footer>Footer</el-footer>
+   <el-footer style="height:13%;">Footer</el-footer>
 </el-container>
 </template>
 <style>
@@ -14,7 +13,7 @@
     background-color: #B3C0D1;
     color: #333;
     text-align: center;
-    height:10%;
+
   }
   .el-aside {
     background-color: #D3DCE6;
@@ -25,6 +24,5 @@
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
-
   }
 </style>
