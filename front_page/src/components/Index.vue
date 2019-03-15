7150cc9790b12937c52c6abf00007e35bcc51d73
zhoujianye <111@123.com>
2019-03-15 17:06:48 +0800

    修改页面

--- front_page/src/components/Index.vue
+++ front_page/src/components/Index.vue
@@ -1,28 +1,68 @@
 <template>
-<el-container style=" position: absolute;top: 0;left: 0;width: 100%; height: 100%;">
-  <el-header style="height:13%;">Header</el-header>
-  <el-container>
-  <el-main>Main</el-main>
-    <el-aside width="200px">Aside</el-aside>
-  </el-container>
-   <el-footer style="height:13%;">Footer</el-footer>
-</el-container>
+	<el-container style=" position: absolute;top: 0;left: 0;width: 100%; height: 100%;">
+		<el-header style="height:13%;"></el-header>
+		<el-container>
+			<!--占位使用，不做任何处理-->
+			<el-aside>
+			</el-aside>
+			<!--占位使用，不做任何处理-->
+			<el-main></el-main>
+			<el-aside >
+				<!--搜索内容开始-->
+				<el-row style="height: 17%;">
+					<el-col :span="12" style="    margin-top: 12%; margin-left: 5%;">						
+							<input type="text" autocomplete="off" placeholder="请输入内容" class="el-input__inner">						
+					</el-col>
+					<el-col :span="12"     style="margin-top: -12.8%;   margin-left: 59%;">
+						
+							<el-button type="primary" icon="el-icon-search">搜索</el-button>
+						
+					</el-col>
+					
+				</el-row>
+			</el-aside>
+			<!--占位使用，不做任何处理-->
+			<el-aside>
+			</el-aside>
+			<!--占位使用，不做任何处理-->
+		</el-container>
+		<el-footer style="height:13%;"></el-footer>
+	</el-container>
 </template>
 <style>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
+	.el-row {
+		margin-left: 0px;
+		margin-right: 0px;
+		margin-top: 10px;
+		height: 30%;
+		background: white;
+		margin-bottom: 20px;
+		&:last-child {
+			margin-bottom: 0;
+		}
+	}
 
-  }
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-  }
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-  }
+	.el-input {
+		height: 10px;
+	}
+	
+	.el-col {
+		border-radius: 4px;
+		margin: auto;
+	}	
+	.el-aside{
+		    overflow: hidden;
+	}
+	body {
+		background: #f6f6f6;
+	}
 </style>
+<script>
+	export default {
+		data() {
+			return {
+				input: ''
+			}
+		}
+	}
+</script>
