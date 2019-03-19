6ec617f71c3fc82e20cb82a828513a02c53c59a9
zhoujianye <111@123.com>
2019-03-19 10:59:00 +0800

    侧边栏组件化

--- front_page/src/components/article_list.vue
+++ front_page/src/components/article_list.vue
@@ -1,5 +1,4 @@
 <template>
-
 	<el-container style=" background-color: white;margin-left: 12%;margin-right: 12%;margin-top: 5%;">
 		<el-header style="height:13%;">
 			<h1><a id="Header1_HeaderTitle" class="headermaintitle" >dagsk的文集</a></h1>
@@ -10,7 +9,6 @@
 		<el-container>
 			<!--占位使用，不做任何处理-->
 			<el-aside>
-
 			</el-aside>
 			<!--占位使用，不做任何处理-->
 			<el-main style="margin-left: -17%;">
@@ -40,147 +38,37 @@
 
 				</div>
 			</el-main>
-			<el-aside>
-				<!--搜索内容开始-->
-				<el-row style="height: 17%;">
-					<el-col :span="12" style="    margin-top: 12%; margin-bottom:-5px;margin-left: 5%;">
-						<input type="text" autocomplete="off" id="search" placeholder="请输入内容" class="el-input__inner">
-					</el-col>
-					<el-col :span="12" style="margin-top: -12%; margin-bottom:-5px; margin-left: 59%;">
-						<el-button type="primary" @click='search_article()' icon="el-icon-search">搜索</el-button>
-					</el-col>
-				</el-row>
-				<!--搜索内容结束-->
-
-				<!--我的分类开始-->
-				<el-row style="margin-top: 7%;height:37%;    margin-left: -43%;">
-					<el-col :span="24" style=" text-align:center;height: 29%;    line-height: 400%;">
-						<div class="grid-content bg-purple-dark">
-							<span style='color: black;font-size: 22px;'>我的分类</span>
-						</div>
-					</el-col>
-					<div v-for="cate in cate_list">
-						<el-col :span="24" class="type_list">
-							<div class="grid-content bg-purple-dark">
-								<span class="type_font" @click='article_list_by_cate(cate.cate_id)'>{{cate.title}}({{cate.article_count}})</span>
-							</div>
-						</el-col>
-					</div>
-				</el-row>
-				<!--我的分类结束-->
-
-				<!--我的标签开始-->
-				<el-row style="height:37%;margin-left: -43%;    margin-top: -15px;">
-					<el-col :span="24" style=" text-align:center;height: 29%;    line-height: 400%;">
-						<div class="grid-content bg-purple-dark" style="margin-left: 10px;">
-							<span style='color: black;font-size: 22px;'>我的标签</span>
-						</div>
-					</el-col>
-					<el-col :span="24" class="type_list" style='margin-left: 40px;'>
-						<div class="grid-content bg-purple-dark">
-							<span id="label_list" v-for="label in label_list" @click='article_list_by_cate(label.label_id)'>
-								{{label.title}}({{label.article_count}}),
-							</span>
-						</div>
-					</el-col>
-				</el-row>
-				<!--我的标签结束-->
-
-				<!--我的经常访问开始-->
-				<el-row style="margin-top: 25%;height:37%;    margin-left: -43%;">
-					<el-col :span="24" style=" text-align:center;height: 29%;    line-height: 400%;">
-						<div class="grid-content bg-purple-dark">
-							<span style='color: black;font-size: 22px;'>经常浏览</span>
-						</div>
-					</el-col>
-					<el-col :span="24" class="type_list">
-						<div class="grid-content bg-purple-dark"><span>我的分类</span></div>
-					</el-col>
-				</el-row>
-				<!--我的经常访问结束-->
-
-			</el-aside>
-			<!--占位使用，不做任何处理-->
-			<el-aside style="width:100px">
-			</el-aside>
-			<!--占位使用，不做任何处理-->
+			<html_aside v-bind:article_list="article_list" v-on:change_articles="article_changed"></html_aside>
 		</el-container>
 		<el-footer style="height:13%;"></el-footer>
 	</el-container>
 </template>
-
-<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
 <script type="text/ecmascript-6">
 	import navigate from '@/components/navigate';
+	import html_aside from '@/components/html_aside';
 	import 'jquery'
 	// 引入外部整理好的css文件
 	import "@/css/article_list.css"
-
-	function privateFoo(data) {
-		this.msg = data
-	}
-	var vue
+	var vue;
 	export default({
 		name: 'App',
 		data() {
 			return {
-				input: '',
-				article_list: [],
-				cate_list: [],
-				label_list: [],
+				article_list: [],				
 			}
 		},
 		components: {
-			navigate: navigate
+			navigate: navigate,
+			html_aside:html_aside
 		},
 		created: function() {
 			vue = this
 		},
-		methods: {
-			article_list_by_cate(cate_id) {
-				$.ajax({
-					type: "GET",
-					url: "http://127.0.0.1:8000/api/article_list",
-					data: {
-						'cate_id': cate_id
-					},
-					dataType: "json",
-					success: function(data) {
-						vue.article_list = data.data
-
-					}
-				});
-			},
-			article_list_by_cate(label_id) {
-				$.ajax({
-					type: "GET",
-					url: "http://127.0.0.1:8000/api/article_list",
-					data: {
-						'label_id': label_id
-					},
-					dataType: "json",
-					success: function(data) {
-						vue.article_list = data.data
-
-					}
-				});
-			},
-			search_article() {
-				var title = $("#search").val()
-				$.ajax({
-					type: "GET",
-					url: "http://127.0.0.1:8000/api/article_list",
-					data: {
-						'title': title
-					},
-					dataType: "json",
-					success: function(data) {
-						vue.article_list = data.data
-
-					}
-				});
+		methods:{
+			article_changed: function(event){
+				this.article_list = event; 
 			}
-		}
+		}		
 	})
 
 	$(document).ready(function() {
@@ -191,26 +79,7 @@
 			dataType: "json",
 			success: function(data) {
 				vue.article_list = data.data
-
 			}
-		});
-		//请求分类列表
-		$.ajax({
-			type: "GET",
-			url: "http://127.0.0.1:8000/api/cate_list",
-			dataType: "json",
-			success: function(data) {
-				vue.cate_list = data.data
-			}
-		});
-		//我的标签
-		$.ajax({
-			type: "GET",
-			url: "http://127.0.0.1:8000/api/label_list",
-			dataType: "json",
-			success: function(data) {
-				vue.label_list = data.data
-			}
-		});
+		});		
 	});
 </script>
