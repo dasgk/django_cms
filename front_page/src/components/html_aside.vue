fae49ef78187ad46a89c9eb053215d3c925f5aad
dasgk <674026565@qq.com>
2019-04-25 22:02:43 +0800

    首页修改

--- front_page/src/components/html_aside.vue
+++ front_page/src/components/html_aside.vue
@@ -1,20 +1,31 @@
 <template>
-	<div>
-		<el-card class="box-card" style="margin-top: 5%;margin-left: 9%;width:84%">
-		</el-card>
-	</div>
-</template>
-<script type="text/ecmascript-6">
-	// 引入外部整理好的css文件
-	import "@/css/html_aside.css"
-	import databus from "@/datacenterbus.js"
-	var vue
-	export default {
-		name: 'html_aside',
-		data() {
-			return {};
-		},
-		methods: {}
+  <div>
+    <el-card id="search_in_site" class="box-card" style="margin-top: 20%;margin-left: 9%;width:84%; text-align: center;">
 
-	}
+      <div slot="header" class="clearfix">
+        <span style="text-align:center">站内搜索</span>
+      </div>
+
+    </el-card>
+  </div>
+</template>
+<style>
+
+
+
+</style>
+<script type="text/ecmascript-6">
+  // 引入外部整理好的css文件
+  import "@/css/html_aside.css"
+  import databus from "@/datacenterbus.js"
+
+  var vue
+  export default {
+    name: 'html_aside',
+    data() {
+      return {};
+    },
+    methods: {}
+
+  }
 </script>
