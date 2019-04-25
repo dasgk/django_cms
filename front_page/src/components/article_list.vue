b5295f41efdb20924e6b3f62055af7e44a006096
zhoujianye <111@123.com>
2019-04-25 16:46:36 +0800

    修改页面

--- front_page/src/components/article_list.vue
+++ front_page/src/components/article_list.vue
@@ -5,17 +5,52 @@
 				<img src="https://www.qdtalk.com/wp-content/uploads/2019/03/break-car-couple-196666-.jpg">
 			</div>
 			<div class='article_list_relative_info'>
+
 				<div class="article_title">
+
 					<!--   这是文章所属分类 -->
-					<a class="label" href="http://www.baidu.com" > <span>Laravel</span> <i class="label-arrow"></i></a>
+					<a class="label" href="http://www.baidu.com"> <span>Laravel</span> <i class="label-arrow"></i></a>
 					<!--   这是文章所属分类 结束-->
+
 					<!---  文章标题   -->
 					<h2><a href="https://www.qdtalk.com/2019/03/31/js%e5%af%bc%e5%85%a5%e5%af%bc%e5%87%ba%e6%80%bb%e7%bb%93%e4%b8%8e%e5%ae%9e%e8%b7%b5/">js导入导出总结与实践</a></h2>
 					<!---  文章标题    结束--->
+
 				</div>
+
+				<!---  文章摘要  -->
+				<div class="article_abstract">
+					<p>
+						&nbsp;&nbsp;&nbsp;&nbsp;这篇文章是对上一篇文章的总结和实践， 当直接给 module.exports时，exports会失效， 直接给exports赋值会切断exports和 module.exports的关联关系， export，export…
+					</p>
+				</div>
+				<!---  文章摘要    结束--->
 			</div>
-		</el-card>		
-	
+			<div class="article_num_info">
+
+				<span class="visible-lg visible-md visible-sm pull-left">
+					<a href="https://www.qdtalk.com/2019/03/24/require%ef%bc%8cexports%ef%bc%8cmodule-exports%e5%92%8ces6%e4%b8%ad%e7%9a%84import%ef%bc%8cexport%ef%bc%8cexport-default/">
+						<i class="el-icon-date"></i> 2019年3月24日</a>
+						&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.qdtalk.com/2019/03/24/require%ef%bc%8cexports%ef%bc%8cmodule-exports%e5%92%8ces6%e4%b8%ad%e7%9a%84import%ef%bc%8cexport%ef%bc%8cexport-default/#respond">
+						<i class="el-icon-upload"></i> 4条评论</a>
+					</span>
+
+				<span class="pull-left">
+						&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.qdtalk.com/2019/03/24/require%ef%bc%8cexports%ef%bc%8cmodule-exports%e5%92%8ces6%e4%b8%ad%e7%9a%84import%ef%bc%8cexport%ef%bc%8cexport-default/">
+						<i class="el-icon-view"></i> 616次阅读</a>
+						&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.qdtalk.com/2019/03/24/require%ef%bc%8cexports%ef%bc%8cmodule-exports%e5%92%8ces6%e4%b8%ad%e7%9a%84import%ef%bc%8cexport%ef%bc%8cexport-default/">
+						<i class="el-icon-star-on"></i> 4人点赞</a>
+		</span>
+				<span class="pull-right">
+			<a class="read-more" href="https://www.qdtalk.com/2019/03/24/require%ef%bc%8cexports%ef%bc%8cmodule-exports%e5%92%8ces6%e4ault/" title="阅读全文">阅读全文	<i class="el-icon-caret-right"></i></a>
+		</span>
+
+			</div>
+		</el-card>
+
 	</div>
 </template>
 <script type="text/ecmascript-6">
