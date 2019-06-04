0aefe5f9b5b30cd94da1dd5f9aec712ba8031b99
dasgk <674026565@qq.com>
2019-06-04 21:54:39 +0800

    设置数据过滤方式

---
+++ python/data_exploer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+'''
+    进行数据探索，检测异常值
+'''
+data_sample_path = './dlj_dlj_order.csv'
+data = pd.read_csv(data_sample_path,index_col='order_status')
+#删除order_amount为0的缺失值
+data = data[data['order_amount']>1]
+#分析异常值，也就是use_count,使用3倍标准差原则，也就是测定值和平均值的偏差超过3倍标准差的话，则视为异常值
+data_std = data.describe()['use_count']['std']
+#再次过滤异常值，使用最大最小值进行异常值顾虑最小是5，最大是8*60*60
+data_min = 5
+data_max = 8*60*60
+
