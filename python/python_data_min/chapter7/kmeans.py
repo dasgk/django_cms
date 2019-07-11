138193c1450c7a306df66471014a72ed6dc61587
dasgk <674026565@qq.com>
2019-07-11 22:01:33 +0800

    第七章数据清洗

--- python/python_data_min/chapter7/kmeans.py
+++ python/python_data_min/chapter7/kmeans.py
@@ -1,11 +1,15 @@
 #-*- coding:utf-8 -*-
 
 import pandas as pd
+# sklearn.cluster 是聚类子库
+from sklearn.cluster import KMeans
 
 inputfile='./data/air_data.csv'
 explorefile = './data/data_after_explore.xls'
 cleanfile = './data/data_after_clean.xls'
 
+input_score = './data/zscoredata.xls'
+output_score = './data/out_zscoreddata.xls'
 
 '''
     以下是数据统计
@@ -15,10 +19,13 @@ def data_statics():
     # percentiles 参数是指定计算多少的分位数表 T是转置方便使用， describe自动计算非空值
     #包括对数据的基本描述   行索引是 count   unique  top  freq
     # 列索引是 每个属性
-    explore = data.describe(percentiles=[], include='all')
+    # percentiles,这个参数可以设定数值型特征的统计量，默认是[.25, .5, .75],
+    # 也就是返回25%，50%，75%数据量时的数字，但是这个可以修改的，如果是空的则返回50%的分位数
     # 行索引就是每个属性的名字了， 列索引自然就是  max  min   null
-    explore = data.describe(percentiles=[], include='all').T
-
+    # include，这个参数默认是只计算数值型特征的统计量，当输入include=['O']，会计算离散型变量的统计特征，all则计算所有
+    #  unique   top  freq
+    #  count:非空值总数   mean:非空值的平均值   unique: 唯一值数   top: 频数最高者  freq: 最高频数
+    explore = data.describe(percentiles=[]).T
     # count是非空值的数量。手动计算空值的数量,对每列进行操作
     explore['null'] = len(data) - explore['count']
     explore = explore[['null','max','min']]
@@ -37,8 +44,24 @@ def data_clean():
     data.to_excel(cleanfile)
 
 
+def data_score():
+    data = pd.read_excel(input_score)
+    #标准化变换
+    data = (data-data.mean(axis=0))/(data-data.std(axis=0))
+    data.columns = ['Z' + i for i in data.columns]
+    data.to_excel(output_score)
 
-data_clean()
+
+def kmeans():
+    data = pd.read_excel(output_score)
+    k = 5
+    kmodel = KMeans(n_clusters=k, n_jobs=4)
+    kmodel.fit(data)
+    print(kmodel.cluster_centers_)
+    print(kmodel.labels_)
+
+data_statics()
+
 
 
 
